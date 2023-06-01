adc6ea736fe9f36ff373953526aab742c53af496
bilal hkm <bilal.hkmsh@gmail.com>
2023-06-01 08:57:01 +0300

    metheods

---
+++ methods/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+namespace methods
+{
+    class program
+    {
+        static void Main(string[] args)
+        {
+            int a = 2;
+            int b = 3;
+            Console.WriteLine(a + b);
+            Console.WriteLine(Topla(a, b));
+            Methods yazdir = new Methods();
+            yazdir.print($"{Topla(a, b)}");
+        }
+        static int Topla(int a, int b)
+        {
+            return (a + b);
+        }
+
+    }
+    class Methods
+    {
+        public void print(string message)
+        {
+            Console.WriteLine(message);
+        }
+    }
+}
