afcc8d766c9996875dbd5ff03b8dd47f7e926374
bilal hkm <bilal.hkmsh@gmail.com>
2023-06-03 01:24:28 +0300

    ready-made

---
+++ ready-made/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.WriteLine("Enter a sentence:");
+string? input = Console.ReadLine();
+string titleCase = input.ToUpper();
+Console.WriteLine(titleCase);
+string lowerCase = input.ToLower();
+Console.WriteLine(lowerCase);
+int length = input.Length;
+Console.WriteLine(length);
+bool contains = input.Contains("b");
+Console.WriteLine(contains);
+string substr = input.Substring(7);
+Console.WriteLine(substr);
+string replaced = input.Replace(substr, "masrs !");
+Console.WriteLine(replaced);
+string concatenated = string.Concat("by Bilal ", input);
+Console.WriteLine(concatenated);
